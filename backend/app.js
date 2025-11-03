5a4f9a215314ddf1bc90424d403637d7205401e2
Tate Woodward <tatewoodward@comcast.net>
2025-11-03 15:37:33 -0800

    cors configured

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,15 @@ import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://elaborate-froyo-0477a8.netlify.app/",
+      "https://localhost:5173",
+     ],
+     methods: ["GET", "POST", "PUT", "DELETE"],
+     credentials: true,
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
